76d18466ee29f71c01f562d31570e55bb36a4171
Viet <viettangle12@gmail.com>
2022-01-24 23:00:22 -0500

    finish table with action icons

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,41 +1,55 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { retrieveBills } from '../actions/bill'
-import { Table } from 'antd';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { retrieveBills } from "../actions/bill";
+import { Table, Space } from "antd";
+import { DeleteTwoTone, EyeTwoTone, EditTwoTone } from "@ant-design/icons";
 
 const Bill = () => {
-    const bills = useSelector(state => state.bills)
-    const dispatch = useDispatch();
+  const bills = useSelector((state) => state.bills);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(retrieveBills());
-    }, []);
+  useEffect(() => {
+    dispatch(retrieveBills());
+  }, []);
 
-    const renderBills = () => {
-        const columns = [
-            {
-                title: 'Name',
-                dataIndex: 'accountName'
-            },
-            {
-                title: 'Company',
-                dataIndex: 'company'
-            },
-            {
-                title: 'Actions',
-                dataIndex: ''
-            }
-        ];
-        const data = bills;
-        return <Table columns={columns} dataSource={data} />;
-    };
+  const renderBills = () => {
+    const columns = [
+      {
+        title: "Name",
+        dataIndex: "accountName",
+      },
+      {
+        title: "Company",
+        dataIndex: "company",
+      },
+      {
+        title: "Actions",
+        dataIndex: "",
+        render: (text, record) => (
+          <Space size="middle">
+            <EyeTwoTone twoToneColor="green" style={{ fontSize: "32px" }} />
+            <EditTwoTone twoToneColor="#ea561b" style={{ fontSize: "32px" }} />
+            <DeleteTwoTone
+              twoToneColor="#cc0000"
+              style={{ fontSize: "32px" }}
+            />
+          </Space>
 
-    return (
-        <div>
-            {renderBills()}
-        </div>
-    )
+          //   <Space size="middle">
+          //     <a>{record.key}</a>
+          //     <a>Delete</a>
+          //   </Space>
+        ),
+      },
+    ];
+    return <Table columns={columns} dataSource={bills} />;
+  };
+
+  return (
+    <div>
+      {renderBills()}
+    </div>
+  );
 };
 
 export default Bill;
--- src/reducers/billReducer.js
+++ src/reducers/billReducer.js
@@ -3,9 +3,10 @@ import { RETRIEVE_BILLS } from "../actions/types";
 const INITIAL_STATE = [];
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
+    const {type, payload} = action;
+    switch (type) {
         case RETRIEVE_BILLS:
-            return [...state, ...action.payload];
+            return [...payload];
         default:
             return state;
     }
