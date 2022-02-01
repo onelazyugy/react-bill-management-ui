3d9b25149e7aabdb22381fbc584a17f217da6fa8
Viet <viettangle12@gmail.com>
2022-01-31 23:22:26 -0500

    displaying record onto modal

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -10,7 +10,7 @@ const Bill = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [isModalVisible, setIsModalVisible] = useState(false);
-
+  const [selectedRecord, setSelectedRecord] = useState(null);
 
   useEffect(() => {
     dispatch(retrieveBills());
@@ -32,13 +32,13 @@ const Bill = () => {
         render: (text, record) => (
           <Space size="middle">
             <Tooltip title="view details">
-              <Button type="link" onClick={showModal}>
+              <Button type="link" onClick={() => showModal(record)}>
                 <EyeTwoTone twoToneColor="green" style={{ fontSize: "32px" }} />
               </Button>
             </Tooltip>
 
             <Tooltip title="edit bill">
-              <Button type="link" onClick={showModal}>
+              <Button type="link" onClick={() => showModal(record)}>
                 <EditTwoTone
                   twoToneColor="#ea561b"
                   style={{ fontSize: "32px" }}
@@ -47,7 +47,7 @@ const Bill = () => {
             </Tooltip>
 
             <Tooltip title="delete bill">
-              <Button type="link" onClick={showModal}>
+              <Button type="link" onClick={() => showModal(record)}>
                 <DeleteTwoTone
                   twoToneColor="#cc0000"
                   style={{ fontSize: "32px" }}
@@ -65,7 +65,8 @@ const Bill = () => {
     history.push("/create");
   };
 
-  const showModal = () => {
+  const showModal = (record) => {
+    setSelectedRecord(record);
     setIsModalVisible(true);
   };
 
@@ -76,6 +77,13 @@ const Bill = () => {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
+
+  const renderSelectedRecordOntoModal = () => {
+    if(selectedRecord !== null) {
+        return(<p>{selectedRecord.accountName}</p>);
+    }
+    return(<p>no record</p>);
+  }
 
   return (
     <div style={{ padding: "5px" }}>
@@ -90,10 +98,14 @@ const Bill = () => {
           NEW BILL
         </Button>
       </div>
-      <Modal title="Basic Modal" maskClosable={false} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Modal
+        title="Basic Modal"
+        maskClosable={false}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div>{renderSelectedRecordOntoModal()}</div>
       </Modal>
     </div>
   );
